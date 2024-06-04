50b460b430ecaf1ecdf499ca17991446d68ab2b6
Ansumane Mané <manoansu@gmail.com>
2024-06-04 14:25:41 +0100

    Adding routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Expenses from "./routes/Expenses/index.tsx";
+import Invoices from "./routes/Invoices/index.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
